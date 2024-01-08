27d8dd4a9c1e80248091a8744d103860f1061bb6
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2024-01-09 00:51:49 +0530

    input from user

---
+++ input.cpp
@@ -0,0 +1,16 @@
+// Taking input from user
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // variable declare
+    string name;
+
+    cout << "Pls enter your name: ";
+    cin >> name;
+
+    cout << "Welcome " << name << endl;
+
+    return 0;
+}
