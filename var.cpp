f4c51471bf0f9adf9eaaab2570417f6c0ed8f108
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2024-01-09 00:53:44 +0530

    Using all data types

---
+++ var.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // variable initialize
+    int age = 10;
+    float pi = 3.14;
+    double d = 1.1516;
+    char alp = 'g';
+    string name = "Mukesh";
+
+    cout << "Name : " << name << endl;
+    cout << "Age : " << age << endl;
+    cout << "pi : " << pi << endl;
+    cout << "double : " << d << endl;
+    cout << "alp : " << alp << endl;
+
+    return 0;
+}
