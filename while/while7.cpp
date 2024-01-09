0a30ecb6781560410894295fa48636081cca3f58
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2024-01-09 23:38:14 +0530

    print sum of 1 to 10

---
+++ while/while7.cpp
@@ -0,0 +1,13 @@
+//Print sum of 1 to 10
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,sum=0;
+    while(i<=10)
+    {
+        sum=sum+i;
+        i++;
+        cout<<sum<<endl;
+    }
+}
