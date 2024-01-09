974abb5b525e57567c68623e9429d44ae281320b
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2024-01-09 22:51:18 +0530

    print n numbers

---
+++ while/while1.cpp
@@ -0,0 +1,17 @@
+// print 1 to n
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i = 1;
+
+    cout << "Pls enter a number: ";
+    cin >> n;
+
+    while(i<=n)
+    {
+        cout << i << endl;
+        i++;
+    }
+}
