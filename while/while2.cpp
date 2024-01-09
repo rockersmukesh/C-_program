fd53990a6d0a74d39102c2806d9948c810d3caef
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2024-01-09 22:52:24 +0530

    print reverse n numbers

---
+++ while/while2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// print first n natural number in reverse order
+// n to 1
+int main()
+{
+    int n;
+    cout << "pls enter number: ";
+    cin >> n;
+
+    while(n>=1)
+    {
+        cout << n << endl;
+        n--;
+    }
+}
