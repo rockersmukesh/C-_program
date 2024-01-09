8a3eb69788f0a74078396cd0a9bed1ef7c3ddb1b
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2024-01-09 22:53:56 +0530

    print first 10 odd number

---
+++ while/while3.cpp
@@ -0,0 +1,14 @@
+// print first 10 odd number
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int i = 1;
+    while(i<=19)
+    {
+        cout << i << endl;
+        i = i + 2;
+    }
+}
