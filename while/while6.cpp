95d418db4c1a1983c95a035af01769f5d8cac113
Rockers Mukesh <110319782+rockersmukesh@users.noreply.github.com>
2024-01-09 23:37:02 +0530

    print square of 1 to 10

---
+++ while/while6.cpp
@@ -0,0 +1,13 @@
+//print square from 1 to 10
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,sq;
+    while(i<=10)
+    {
+        sq=i*i;
+        cout<<sq<<endl;
+        i++;
+    }
+}
